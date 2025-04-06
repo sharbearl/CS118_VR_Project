2151df418856c133dc1ee4c0464944b1cb8defe3
sharbearl <sharonleo.hearts@gmail.com>
2025-04-06 11:49:15 -0700

    Added keybinds to return to main menu

---
+++ Assets/Scripts/basicInputs.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class basicInputs : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("Pressed");
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
