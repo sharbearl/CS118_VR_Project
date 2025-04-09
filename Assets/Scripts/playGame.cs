cbbfc4a3b7cfe892ee8bff3ab566c3a0951e8da6
sharbearl <sharonleo.hearts@gmail.com>
2025-04-09 09:27:07 -0700

    Added code to display cursor image and fixed scene change bug

--- Assets/Scripts/playGame.cs
+++ Assets/Scripts/playGame.cs
@@ -1,16 +1,36 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class playGame : MonoBehaviour
 {
+    public Image startImg;
+    public Image quitImg;
+
+    public void Start()
+    {
+        startImg.enabled = false;
+        quitImg.enabled = false;
+    }
+
     public void Play()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("MainScene");
     }
 
     public void Quit()
     {
         Debug.Log("quitting game");
         Application.Quit();
+    }
+
+    public void ToggleStart()
+    {
+        startImg.enabled = !startImg.enabled;
+    }
+
+    public void ToggleQuit()
+    {
+        quitImg.enabled = !quitImg.enabled;
     }
 }
