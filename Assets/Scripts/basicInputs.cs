6889d1253fbc82be98eee01c3c4e3d0be60961c5
sharbearl <sharonleo.hearts@gmail.com>
2025-04-09 18:08:22 -0700

    Fixed main menu inputs

--- Assets/Scripts/basicInputs.cs
+++ Assets/Scripts/basicInputs.cs
@@ -6,7 +6,7 @@ public class basicInputs : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.None;
     }
 
     // Update is called once per frame
