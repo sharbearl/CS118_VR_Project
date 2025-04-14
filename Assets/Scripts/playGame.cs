b4b028a49e581b434450a166d6baf84d159d53f1
sharbearl <sharonleo.hearts@gmail.com>
2025-04-14 13:02:15 -0700

    Updated main menu assets

--- Assets/Scripts/playGame.cs
+++ Assets/Scripts/playGame.cs
@@ -1,16 +1,37 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class playGame : MonoBehaviour
 {
+    public Image startImg;
+    public Image quitImg;
+
+    public void Start()
+    {
+        startImg.enabled = false;
+        quitImg.enabled = false;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void Play()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("MainScene");
     }
 
     public void Quit()
     {
         Debug.Log("quitting game");
         Application.Quit();
+    }
+
+    public void ToggleStart()
+    {
+        startImg.enabled = !startImg.enabled;
+    }
+
+    public void ToggleQuit()
+    {
+        quitImg.enabled = !quitImg.enabled;
     }
 }
