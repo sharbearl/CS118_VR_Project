a388dde2f09028078e665473ee5fa5a70f1fe024
sharbearl <sharonleo.hearts@gmail.com>
2025-05-20 14:29:10 -0700

    Added sound effect for light switch

--- Assets/Scripts/lights.cs
+++ Assets/Scripts/lights.cs
@@ -5,6 +5,8 @@ public class lights : MonoBehaviour, Interactable
 {
     private List<GameObject> light = new List<GameObject>();
     private bool lightsOn = true;
+    public AudioSource lightSwitchOn;
+    public AudioSource lightSwitchOff;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,10 +26,24 @@ public class lights : MonoBehaviour, Interactable
     public void Interact()
     {
         lightsOn = !lightsOn;
+        PlaySound();
         Debug.Log("turning lights " + (lightsOn ? "on" : "off"));
+
         foreach (GameObject obj in light)
         {
             obj.SetActive(lightsOn);
+        }
+    }
+
+    private void PlaySound()
+    {
+        if (lightsOn)
+        {
+            lightSwitchOn.Play();
+        }
+        else
+        {
+            lightSwitchOff.Play();
         }
     }
 }
